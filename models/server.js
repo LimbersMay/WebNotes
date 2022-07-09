203a4bd2e2543695ccf46fc8d7f4f08df3d5907e
limbersMay <limbermay1@gmail.com>
2022-07-09 15:14:15 -0500

    se agregó la clase de server en donde se manejará todo del servidor

---
+++ models/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const hbs = require('hbs');
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Handlebars config
+        hbs.registerPartials(__dirname + "/views/partials");
+        this.app.set('view engine', 'hbs');
+
+        // Rutas
+        this.authPath = '/api/auth/';
+        
+        // Conectar la base de datos
+
+        // Iniciar los middlewares
+        this.middlewares();
+
+        // Iniciar las rutas de la aplicación
+        this.routes();
+    }
+
+    async connectDB() {
+
+    }   
+    
+    middlewares() {
+
+        // Cors
+        this.app.use(cors());
+
+        // Body parsing
+        this.app.use(express.json());
+
+        // Public directory 
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use(this.authPath, require('../routes/auth'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servicor corriendo en el puerto: ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
