1976d5862b2099eb23ffb365321c2ccf6c4e0647
limbersMay <limbermay1@gmail.com>
2022-07-12 15:11:34 -0500

    se agregó la plantilla de la función para registrarse

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,21 @@ const bcrypt = require('bcryptjs');
 const { response, request } = require('express');
 
 const login = async(req = request, res = response) => {
-
+    
+    // Obtenemos el correo y contraseña del body 
     const { email, password } = req.body;
 
     res.json({
         userEmail: email,
         userPassword: password
     })
+};
 
+const signUp = async(req, res) => {
+    
 };
 
 module.exports = {
-    login
+    login,
+    signUp
 }
