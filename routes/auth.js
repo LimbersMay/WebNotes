2dd7376c710831e49d4cc08475e3f14a0730b9bc
limbersMay <limbermay1@gmail.com>
2022-07-12 15:11:58 -0500

    se agregó la ruta /signup para registrarse

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, signUp } = require('../controllers/auth');
 const validarCampos = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -12,5 +12,7 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/signup', [], signUp);
 
 module.exports = router;
