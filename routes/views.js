e57221233c8cf16662551cdaec0d4ca1afc8cbe3
limbersMay <limbermay1@gmail.com>
2022-07-12 23:03:35 -0500

    se recibieron los cookies con los errores al ingresar información

---
+++ routes/views.js
@@ -0,0 +1,40 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { login, signIn } = require('../controllers/views');
+
+const router = Router();
+
+router.get('/login', (req, res) => {
+
+    if (req.cookies["Errors"]) {
+        const errors = req.cookies["Errors"];
+        res.clearCookie("Errors", { httpOnly: true });
+        console.log('Cookie received: ', req.cookies);
+
+        return res.render('login', {
+            tittle: 'Considere lo siguiente: ',
+            errors: errors
+        });
+    }
+
+    res.render('login');
+});
+
+router.get('/signin', (req, res) => {
+
+    if (req.cookies["Errors"]) {
+        const errors = req.cookies["Errors"];
+        res.clearCookie("Errors", { httpOnly: true });
+        console.log('Cookie received: ', req.cookies);
+
+        return res.render('signin', {
+            tittle: 'Considere lo siguiente: ',
+            errors: errors
+        });
+    }
+
+    res.render('signin');
+});
+
+module.exports = router;
