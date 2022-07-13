f12fef17f3e9b568f7cebfb19ffc25662b280a8f
limbersMay <limbermay1@gmail.com>
2022-07-12 23:04:09 -0500

    se agregaron las validaciones para los campos de registro de usuario

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,26 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login, signUp } = require('../controllers/auth');
+const { login, signIn } = require('../controllers/auth');
+const { existeEmail } = require('../helpers/db-validators');
+const validatePassword = require('../middlewares/password-validator');
 const validarCampos = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post('/login', [
-    check('email', 'El correo es obligatorio').isEmail(),
-    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('password', 'La contraseña no puede estar vacia').not().isEmpty(),
     validarCampos
 ], login);
 
-router.post('/signup', [], signUp);
+router.post('/signin', [
+    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('email').custom(existeEmail),
+    check('password').custom(validatePassword),
+    check('password', 'La longitud mínima de la contraseña es 8').isLength({min: 8}),
+    validarCampos
+], signIn);
 
 module.exports = router;
