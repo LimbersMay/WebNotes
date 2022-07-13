d01c569870ae1247c6d2d1dcead488d4763e3c7e
limbersMay <limbermay1@gmail.com>
2022-07-12 21:45:06 -0500

    se agregó una cookie al response para procesarla posteriormente

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -5,12 +5,17 @@ const { validationResult } = require('express-validator');
 const validarCampos = (req, res = response, next) => {
 
     const errors = validationResult(req);
+
+    const listErrors = []; 
+    
+    errors.array().forEach(dict => {
+        listErrors.push(dict.msg); 
+    });
     
     if (!errors.isEmpty()) {
 
-        return res.render('login', {
-            message: 'Wrong password or email'
-        });
+        res.cookie('Errors', listErrors, {httpOnly:true});
+        return res.redirect(req.get('referer'));
     }
 
     next();
