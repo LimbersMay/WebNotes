02d4e0771b8cc02edf4dc1929f86c029c01fffa2
limbersMay <limbermay1@gmail.com>
2022-07-15 17:09:33 -0500

    se hicieron comprobaciones para ver si el usuario existe en la base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
-const bcryptjs = require('bcryptjs');
 const { response, request } = require('express');
+
+const bcryptjs = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -8,10 +9,24 @@ const login = async(req = request, res = response) => {
     // Obtenemos el correo y contraseña del body 
     const { email, password } = req.body;
 
-    res.json({
-        userEmail: email,
-        userPassword: password
-    })
+    // Comprobamos que el correo esté registrado
+    const usuario = await User.findOne({email});
+
+    if (!usuario) {
+        res.cookie('Errors', 'Este correo no está registrado', {httpOnly:true});
+        return res.status(401).redirect('/user/login')
+    }
+
+    // Si existe el usuario, comprobamos que coincida la contraseña
+    const match = bcryptjs.compareSync(password, usuario.password);
+
+    if (!match) {
+        res.cookie('Errors', 'La contraseña no es correcta', {httpOnly:true});
+        return res.status(401).redirect('/user/login');
+    }
+
+    // Si pasa por todas las comprobaciones, renderizamos la página de home
+    res.status(200).redirect('/user/home');
 };
 
 const signIn = async(req, res = response) => {
