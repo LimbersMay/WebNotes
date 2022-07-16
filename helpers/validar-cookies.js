fe7613509fb1e096568bd34349a5b6b260863628
limbersMay <limbermay1@gmail.com>
2022-07-15 23:18:37 -0500

    se creó la función para validar las cookies y verificar si contiene errores

--- helpers/validar-cookies.js
+++ helpers/validar-cookies.js
@@ -1,16 +1,11 @@
 
-const validarCookies = (req, res, view) => {
-
-    console.log('Referencia: ', req.get('referer'))
+const validarCookies = (req, res) => {
 
     if (req.cookies["Errors"] != undefined) {
         const errors = req.cookies["Errors"];
         res.clearCookie("Errors", { httpOnly: true });
 
-        return res.render(view, {
-            tittle: 'Considere lo siguiente: ',
-            errors: errors
-        });
+        return errors;
     }
 }
 
