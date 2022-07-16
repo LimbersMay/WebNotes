8fa9b32207e9a9fd9850ba12ffe21e6c976431f3
limbersMay <limbermay1@gmail.com>
2022-07-15 23:16:45 -0500

    se agregó el middleware para verificar si se hizo o no un inicio de sesión exitoso

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { passport } = require('../config/passport');
 
 const { login, signIn } = require('../controllers/auth');
 const { existeEmail } = require('../helpers/db-validators');
@@ -10,17 +11,22 @@ const validarCampos = require('../middlewares/validar-campos');
 const router = Router();
 
 router.post('/login', [
-    check('email', 'El correo no es válido').isEmail(),
-    check('password', 'La contraseña no puede estar vacia').not().isEmpty(),
-    validarCampos
-], login);
+    check('username', 'Invalid email').isEmail(),
+    check('password', 'Password cannot be empty').not().isEmpty(),
+    validarCampos,
+    passport.authenticate('local', {
+        successRedirect: '/user/home',   
+        failureRedirect: '/user/login',
+        failureFlash: true
+    })
+]);
 
 router.post('/signin', [
-    check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
-    check('email', 'El correo no es válido').isEmail(),
+    check('username', 'Username is neccesary').not().isEmpty(),
+    check('email', 'Invalid email').isEmail(),
     check('email').custom(existeEmail),
     check('password').custom(validatePassword),
-    check('password', 'La longitud mínima de la contraseña es 8').isLength({min: 8}),
+    check('password', 'The maximun password length is 8').isLength({min: 8}),
     validarCampos
 ], signIn);
 
