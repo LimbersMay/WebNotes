d4ccfcc85ed3b1276788e0b6e17b35d5be654d67
limbersMay <limbermay1@gmail.com>
2022-07-19 10:10:24 -0500

    se agregó la función para agregar la nota en la base de datos y regresar su id

--- controllers/note.js
+++ controllers/note.js
@@ -1,13 +1,39 @@
+const { request } = require('express');
+const User = require('../models/user');
 
 // Métodos de las notas para guardar y eliminar
-const saveNote = (req = Request, res) => {
-    
+const saveNote = async(req = request, res) => {
+
+    // Obtenemos al usuario autenticado 
+    const { _id } = req.user;
+
+    // Creamos un nuevo elemento de Nota
     const { title, content, date } = req.body; 
-        
-    return res.status(200).json({
+
+    const note = {
         title,
         content,
         date
+    }
+
+    // Agregamos la nota en el arreglo de notas del usuario
+    const user = await User.findOneAndUpdate(
+        {_id}, // Filtro
+        {
+            $push: { // Hacemos un push en la posición 0 del elemento
+                notes: {
+                    $each: [note],
+                    $position:0
+                }
+        }}, 
+        {new: true}
+    );
+
+    // Obtenemos el ID del último elemento modificado 
+    const lastNoteId = user.notes[0]._id;
+
+    return res.status(200).json({
+        id: lastNoteId
     });
 }
 
