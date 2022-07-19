ea5a16ce1163d2c14b43c0f5361e5f7645f87b2a
limbersMay <limbermay1@gmail.com>
2022-07-19 13:15:19 -0500

    se agregó el validador para comprobar que no hayan errores en la nota

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -21,7 +21,19 @@ const validarCampos = (req, res = response, next) => {
     next();
 };
 
+const validarNota = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors
+        });
+    }
+
+    next();
+}
 
 module.exports = {
-    validarCampos
+    validarCampos,
+    validarNota
 };
