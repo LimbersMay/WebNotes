24bf84291ded5c7e021e6c7a33e2d706fdebc0cf
limbersMay <limbermay1@gmail.com>
2022-07-19 13:14:39 -0500

    se agregaron middlewares para la petición de guardado de notas

--- routes/note.js
+++ routes/note.js
@@ -1,17 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { saveNote } = require('../controllers/note');
+const { estaAutenticado } = require('../middlewares/note-validator');
+const { validarNota } = require('../middlewares/validar-campos');
 
 // Método para guardar una nota en bd
 const router = Router();
 
-router.post('/saveNote', (req, res, next) => {
-
-    // Verificamos que el usuario esté autenticado
-    if (req.isAuthenticated()) return next();
-    res.redirect('/user/login');
-
-}, saveNote);
+router.post('/saveNote', [
+    estaAutenticado,
+    check('idNote', 'No es un Id de mongo válido').isMongoId(),
+    validarNota
+] , saveNote);
 
 
 module.exports = router;
