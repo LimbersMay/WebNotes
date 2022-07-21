af091bf8e263f697b1f960ce1844803903770d94
limbersMay <limbermay1@gmail.com>
2022-07-21 18:06:01 -0500

    se formateó la fecha

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -5,3 +5,29 @@ const fechaInput = document.getElementsByClassName('date__hidden')[0];
 // Le incrustamos la zona actual en el input oculto
 const zonaActual = Intl.DateTimeFormat().resolvedOptions().timeZone;
 fechaInput.value = zonaActual;
+
+// Formateamos la fecha actual
+const formatDate = (date) => {
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let dateFormatted = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() + " " + (date.getHours() % 12 || 12) + ":";
+
+    if (date.getMinutes() < 10) {
+        dateFormatted += "0" + date.getMinutes();
+    }
+    else {
+        dateFormatted += date.getMinutes();
+    }
+
+    if (date.getHours() >= 12) {
+        dateFormatted += " PM";
+    }
+    else {
+        dateFormatted += " AM";
+    }
+
+    return dateFormatted;
+}
+
+export default formatDate;
