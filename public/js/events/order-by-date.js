023e6e6018c1261a4a513d68c78816c7f812e075
limbersMay <limbermay1@gmail.com>
2022-07-21 11:44:38 -0500

    se ordenaron los elementos de acuerdo a sus fechas

--- public/js/events/order-by-date.js
+++ public/js/events/order-by-date.js
@@ -4,28 +4,36 @@
     { elemento: date }
 */
 
-let elements = {
-
-};
-
 const orderDates = () => {
 
     // Obtenemos todos los elementos de nota del DOM
-    const dateElements = document.getElementsByClassName('note__date');
+    const dateElements = document.getElementsByClassName('notes__note');
 
-    let dates = [];
+    /* Fechas
+        {
+            idElemento: fecha,
+            idElemento: fecha,
+        }
+    */
 
-    // Recorremos todos los elementos de fecha
+    let dates = {};
+
+    // Guardamos en un arreglo sus atributos de fecha
     for (let i = 0; i < dateElements.length; i++) {
-        elements[dateElements[i].parentElement] = Date.parse(dateElements[i].innerHTML).toLocaleString();
-        dates.push(Date.parse(dateElements[i].innerHTML).toLocaleString());
+        dates[dateElements[i].id] = dateElements[i].getAttribute('modified_at');;
     }
 
-    // Ordenamos los elementos de fecha
-    dates.sort((a, b) => a - b);
+    // Ordenamos los elementos de fecha del más nuevo al más viejo
+    dates = Object.keys(dates).sort((a, b) => {
+        return new Date(dates[b]) - new Date(dates[a]);
+    });
 
-    console.log(elements);
-    console.log(dates);
+    // Ordenamos los elementos de nota
+    dates.forEach((id, i) => {
+        // Obtenemos el elemento de la nota
+        const note = document.getElementById(id);
+        note.style.order = i;
+    }); 
 }
 
 
