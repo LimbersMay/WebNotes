6ffb2a86764f274b42009afdffb0309197025c2c
limbersMay <limbermay1@gmail.com>
2022-07-21 14:11:35 -0500

    se devolvió una promesa con la conexión

--- db/config.js
+++ db/config.js
@@ -3,12 +3,18 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
 
-    try {
-        await mongoose.connect(process.env.MONGODB_CNN);
-        console.log('Bases de datos online');
-    } catch (error) {
-        console.log('No se pudo conectar a la base de datos')
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            mongoose.connect(process.env.MONGODB_CNN);
+            console.log('MongoDB connected');
+            resolve('Bases de datos online');
+        }
+
+        catch (error) {
+            console.log("MongoDB didn't can connect");
+            reject('No se pudo conectar a la base de datos')
+        } 
+    });
 }
 
 module.exports = dbConnection;
