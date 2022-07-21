832953a2004d068cdfa0b5c254b401d693c159d0
limbersMay <limbermay1@gmail.com>
2022-07-21 18:47:26 -0500

    se incrustó la fecha solo si existe el input

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -2,9 +2,11 @@
 // Obtenemos el input oculto de la fecha 
 const fechaInput = document.getElementsByClassName('date__hidden')[0];
 
-// Le incrustamos la zona actual en el input oculto
-const zonaActual = Intl.DateTimeFormat().resolvedOptions().timeZone;
-fechaInput.value = zonaActual;
+if (fechaInput !== undefined) {
+    // Le incrustamos la zona actual en el input oculto
+    const zonaActual = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    fechaInput.value = zonaActual;
+}
 
 // Formateamos la fecha actual
 const formatDate = (date) => {
