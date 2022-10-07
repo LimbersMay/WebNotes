5b7e2e05f61e06782166d5e04d3ed1f690a553b7
limbersMay <limbermay1@gmail.com>
2022-10-07 16:50:49 -0500

    se creó la ruta para guardar las opciones del usuario

--- routes/user-settings.js
+++ routes/user-settings.js
@@ -1,4 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { saveSettings } = require('../controllers/user-settings');
+const { existeEmail, existeNombreUsuario } = require('../helpers/db-validators');
+const { estaAutenticado, validarErrores } = require('../middlewares/note-validator');
+
+const router = Router();
+
+router.put('/saveSettings', [
+    estaAutenticado,
+    check('email').isEmail(),
+    check('email').custom(existeEmail),
+    check('username').custom(existeNombreUsuario),
+    validarErrores
+], saveSettings)
 
 module.exports = Router;
