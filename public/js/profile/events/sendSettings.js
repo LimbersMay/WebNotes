d160c09071d438bc319cb75aac217af3712689cf
limbersMay <limbermay1@gmail.com>
2022-10-08 17:38:08 -0500

    se manejaro los errores del servidor y del cliente

--- public/js/profile/events/sendSettings.js
+++ public/js/profile/events/sendSettings.js
@@ -1,12 +1,18 @@
 import { updateAccountChangesAPI, updateAccountPasswordAPI } from "../api/updateSettings.js";
+import { handlePasswordError } from "../messageHandling/sendSettings.js";
 
-const sendAccountChanges = async(event) => {
+const handleMessage = ( element, classList, message ) => {
+    element.classList.add(classList);
+    element.innerHTML = message;
+}
+
+const sendAccountChanges = async (event) => {
 
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const bodyRequest = Object.fromEntries(formData);
-    
+
     const response = await updateAccountChangesAPI(bodyRequest);
     console.log(response);
 }
@@ -15,11 +21,27 @@ const sendAccountPassword = async( event ) => {
 
     event.preventDefault();
 
+    // Obtenemos el elemento de mensaje de contraseña
+    const passwordMessageHtml = event.target.getElementsByClassName('password__message')[0];
+
     const formData = new FormData(event.target);
     const bodyRequest = Object.fromEntries(formData);
 
-    const response = await updateAccountPasswordAPI(bodyRequest);
-    console.log(response);
+    console.log(bodyRequest);
+
+    // Verificamos que ambas contraseñas coincidan
+    if ( bodyRequest.newPassword !== bodyRequest.repeatPassword ) {
+        handleMessage(passwordMessageHtml, 'password__exception', 'Passwords must be equal')
+        return;
+    }
+
+    try {
+        const response = await updateAccountPasswordAPI(bodyRequest);
+        handleMessage(passwordMessageHtml, 'password__successful', response.msg);
+
+    } catch( error ) {
+        handleMessage(passwordMessageHtml, 'password__exception', error.msg);
+    }
 }
 
 export {
