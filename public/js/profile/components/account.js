c1885cc4a71d0614075e6000f36fd420474e0185
limbersMay <limbermay1@gmail.com>
2022-10-08 10:40:25 -0500

    se convirtió el string de HTML directamente a HTML y se le agregó su respectivo evento

--- public/js/profile/components/account.js
+++ public/js/profile/components/account.js
@@ -1,70 +1,83 @@
+import { sendAccountChanges } from "../events/sendSettings.js";
 
 // User account options HTML
 const getAccountHtml = () => {
 
-    return (`
-                <section class="account__settings">
-                    <div class="user__settings">
-                        <div class="setting">
-                            <label class="setting__title" for="username">Username</label><br>
-                            <input class="setting__input" name="username" type="text">
-                        </div>
-
-                        <div class="setting">
-                                <label class="setting__title" for="email">Email</label><br>
-                                <input class="setting__input" name="email" type="email">
-                        </div>
+    const htmlElement = 
+        `
+            <section class="account__settings">
+                <div class="user__settings">
+                    <div class="setting">
+                        <label class="setting__title" for="username">Username</label><br>
+                        <input class="setting__input username" name="username" type="text">
                     </div>
 
-                    <div class="user__preferences">
-                        <div class="setting">
-                            <label class="setting__title" for="language">Language</label><br>
+                    <div class="setting">
+                            <label class="setting__title" for="email">Email</label><br>
+                            <input class="setting__input email"  name="email" type="email">
+                    </div>
+                </div>
 
-                                <select class="setting__input" name="language" id="language">
-                                    <option value="en">English</option>
-                                    <option value="es">Spanish</option>
-                                </select>
-                        </div>
+                <div class="user__preferences">
+                    <div class="setting">
+                        <label class="setting__title" for="language">Language</label><br>
 
-                        <div class="setting">
-                                <label class="setting__title" for="timezone">Timezone</label><br>
-
-                                <select class="setting__input" name="timezone" id="timezone">
-                                    <option value="GMT-12:00">GMT-12:00</option>
-                                    <option value="GMT-11:00">GMT-11:00</option>
-                                    <option value="GMT-10:00">GMT-10:00</option>
-                                    <option value="GMT-09:00">GMT-09:00</option>
-                                    <option value="GMT-08:00">GMT-08:00</option>
-                                    <option value="GMT-07:00">GMT-07:00</option>
-                                    <option value="GMT-06:00">GMT-06:00</option>
-                                    <option value="GMT-05:00">GMT-05:00</option>
-                                    <option value="GMT-04:00">GMT-04:00</option>
-                                    <option value="GMT-03:00">GMT-03:00</option>
-                                    <option value="GMT-02:00">GMT-02:00</option>
-                                    <option value="GMT-01:00">GMT-01:00</option>
-                                    <option value="GMT+00:00">GMT+00:00</option>
-                                    <option value="GMT+01:00">GMT+01:00</option>
-                                    <option value="GMT+02:00">GMT+02:00</option>
-                                    <option value="GMT+03:00">GMT+03:00</option>
-                                    <option value="GMT+04:00">GMT+04:00</option>
-                                    <option value="GMT+05:00">GMT+05:00</option>
-                                    <option value="GMT+06:00">GMT+06:00</option>
-                                    <option value="GMT+07:00">GMT+07:00</option>
-                                    <option value="GMT+08:00">GMT+08:00</option>
-                                    <option value="GMT+09:00">GMT+09:00</option>
-                                    <option value="GMT+10:00">GMT+10:00</option>
-                                    <option value="GMT+11:00">GMT+11:00</option>
-                                    <option value="GMT+12:00">GMT+12:00</option>
-                                </select>
-                        </div>
+                            <select class="setting__input language" name="language">
+                                <option value="en-US">English</option>
+                                <option value="es-ES">Spanish</option>
+                            </select>
                     </div>
 
-                    <div class="setting__buttons">
-                        <button class="setting__save">Update</button>
-                        <button class="setting__cancel">Cancel</button>
+                    <div class="setting">
+                            <label class="setting__title" for="timezone">Timezone</label><br>
+
+                            <select class="setting__input timezone" name="timezone">
+                                <option value="GMT-12:00">GMT-12:00</option>
+                                <option value="GMT-11:00">GMT-11:00</option>
+                                <option value="GMT-10:00">GMT-10:00</option>
+                                <option value="GMT-09:00">GMT-09:00</option>
+                                <option value="GMT-08:00">GMT-08:00</option>
+                                <option value="GMT-07:00">GMT-07:00</option>
+                                <option value="GMT-06:00">GMT-06:00</option>
+                                <option value="GMT-05:00">GMT-05:00</option>
+                                <option value="GMT-04:00">GMT-04:00</option>
+                                <option value="GMT-03:00">GMT-03:00</option>
+                                <option value="GMT-02:00">GMT-02:00</option>
+                                <option value="GMT-01:00">GMT-01:00</option>
+                                <option value="GMT+00:00">GMT+00:00</option>
+                                <option value="GMT+01:00">GMT+01:00</option>
+                                <option value="GMT+02:00">GMT+02:00</option>
+                                <option value="GMT+03:00">GMT+03:00</option>
+                                <option value="GMT+04:00">GMT+04:00</option>
+                                <option value="GMT+05:00">GMT+05:00</option>
+                                <option value="GMT+06:00">GMT+06:00</option>
+                                <option value="GMT+07:00">GMT+07:00</option>
+                                <option value="GMT+08:00">GMT+08:00</option>
+                                <option value="GMT+09:00">GMT+09:00</option>
+                                <option value="GMT+10:00">GMT+10:00</option>
+                                <option value="GMT+11:00">GMT+11:00</option>
+                                <option value="GMT+12:00">GMT+12:00</option>
+                            </select>
                     </div>
-                </section>
-        `)
+                </div>
+
+                <div class="setting__buttons">
+                    <button class="setting__save">Update</button>
+                    <button class="setting__cancel">Cancel</button>
+                </div>
+            </section>
+        `
+        
+    let parser = new DOMParser();
+    let parsedHtml = parser.parseFromString(htmlElement, 'text/html').body;
+
+    parsedHtml = parsedHtml.getElementsByClassName('account__settings')[0];
+    
+    // Le agregamos el evento al botón de actualizar los datos
+    parsedHtml.getElementsByClassName('setting__save')[0]
+    .addEventListener('click', ( event ) => sendAccountChanges( event, parsedHtml ))
+
+    return parsedHtml;
 }
 
 export {
