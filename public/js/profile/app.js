371a428ead21d439c944d3ad1a7df1c15c8cc198
limbersMay <limbermay1@gmail.com>
2022-10-08 18:09:51 -0500

    se simuló un click en el apartado de account

--- public/js/profile/app.js
+++ public/js/profile/app.js
@@ -6,6 +6,9 @@ import { showAccountContent, showPasswordContent } from "./events/select-option.
 const accountContainer = document.getElementsByClassName('account')[0];
 accountContainer.addEventListener('click', showAccountContent);
 
+// Se simulará un click en el apartado de perfil solo si se encuentra actualmente en un dispositivo grande
+if (screen.width > 500) accountContainer.click();
+
 /* Evento del elemento de password */
 const passwordContainer = document.getElementsByClassName('password')[0];
 passwordContainer.addEventListener('click', showPasswordContent);
