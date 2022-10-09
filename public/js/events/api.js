1cd829d1e50b74f5885f3ceab5f530ed5dc74e2b
limbersMay <limbermay1@gmail.com>
2022-10-09 00:20:39 -0500

    se eliminó el envio dela fecha del cliente al servidor

--- public/js/events/api.js
+++ public/js/events/api.js
@@ -7,14 +7,10 @@ let apiUrl = (window.location.hostname.includes('localhost'))
 const saveNoteDb = (idNote, title, content) => {
     // Enviamos los datos usando fetch 
 
-    // Fecha para el servidor
-    let dateClient = new Date().toISOString();
-
     const body = {
         idNote,
         title,
-        content,
-        dateClient
+        content
     };
 
     return new Promise(async(resolve, reject) => {
