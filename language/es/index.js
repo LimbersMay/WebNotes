796df2ab3610de0c33ccf501860859b383ece441
limbersMay <limbermay1@gmail.com>
2022-10-09 22:45:37 -0500

    se agregó las traducciones en inglés y español de los elementos del perfil

--- language/es/index.js
+++ language/es/index.js
@@ -2,8 +2,13 @@ const home = require('./home.json');
 const login = require('./login.json');
 const signIn = require('./signin.json');
 
+const userAccount = require('./profile/account.json');
+const userPassword = require('./profile/password.json');
+
 module.exports = {
     home,
     login,
-    signIn
+    signIn,
+    userAccount,
+    userPassword
 };
