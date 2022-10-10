81dfda4134766316140b28553ce41c001e58367b
limbersMay <limbermay1@gmail.com>
2022-10-09 22:47:42 -0500

    se agregó la función para obtener el diccionario de los elementos del perfil según la preferencia del usuario

--- public/js/profile/api/getSettings.js
+++ public/js/profile/api/getSettings.js
@@ -3,11 +3,17 @@ let apiUrl = (window.location.hostname.includes('localhost'))
     ? 'http://localhost:8080'
     : 'https://webnoteseasy.herokuapp.com';
 
-const getAccountDataAPI = async () => {
+const getAccountDataAPI = async() => {
     const response = await fetch(apiUrl + '/api/user-config/get-config');
     return response.json();
 }
 
+const getAccountLangDictionaryAPI = async( component ) => {
+    const response = await fetch(apiUrl + `/api/user-config/get-langDictionary/${ component }`);
+    return response.json();
+}
+
 export {
-    getAccountDataAPI
+    getAccountDataAPI,
+    getAccountLangDictionaryAPI
 }
