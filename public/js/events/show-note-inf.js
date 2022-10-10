0cf79bc9f0b8f332fb48114741548f8529ff8cc9
limbersMay <limbermay1@gmail.com>
2022-10-10 11:36:21 -0500

    se agregó el botón de cancelar en pantalla si el usuario se encuentra en un teléfono

--- public/js/events/show-note-inf.js
+++ public/js/events/show-note-inf.js
@@ -1,3 +1,4 @@
+import { targetElement } from '../helpers/target-element.js';
 import { getNoteInformation } from './get-note-inf.js';
 import makeVisible from './make-visible.js';
 
@@ -17,6 +18,14 @@ const showInput = (event = event) => {
     // Verificamos que la pantalla este debajo de los 500pxs
     if (screen.width < 500) {
         makeVisible('input');
+
+        // Hacemos visible el botón de cancelar
+        const cancelBtn = document.getElementsByClassName('note__cancel')[0];
+
+        const buttonsContainer = document.getElementsByClassName('note__buttons')[0];
+
+        targetElement(cancelBtn, null, 'visible');
+        buttonsContainer.style.justifyContent = 'center';
     }
 
     // CASO 2. EN CASO DE ESTAR EN UNA COMPUTADORA, QUEREMOS MOSTRAR EL CONTENIDO DE LA TAREA DEL LADO DERECHO 
