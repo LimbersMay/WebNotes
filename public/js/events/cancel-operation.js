38ae173695806cc49e21fe6ffed748b994e23a02
limbersMay <limbermay1@gmail.com>
2022-10-10 11:35:46 -0500

    se agregó el evento para el botón cancelar

---
+++ public/js/events/cancel-operation.js
@@ -0,0 +1,16 @@
+import makeVisible from "./make-visible.js";
+
+
+const cancelUpdate = () => {
+
+    // Comprobamos si nos encontramos en un dispositivo pequeño
+    if (screen.width < 500) {
+        
+        makeVisible('list');
+    }
+
+}
+
+export {
+    cancelUpdate
+}
