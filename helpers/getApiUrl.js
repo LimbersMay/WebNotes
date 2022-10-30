4eece2da7a612e5ba6da11c57e39752c493f1eec
limbersMay <limbermay1@gmail.com>
2022-10-30 13:56:23 -0600

    se eliminó la función que devolvia la URL de la API

--- helpers/getApiUrl.js
+++
@@ -1,8 +0,0 @@
-module.exports = getApiUrl = () => {
-
-    if (process.env.NODE_ENV === 'development') {
-        return 'http://localhost';
-    }
-
-    return 'https://webnoteseasy.herokuapp.com';
-}
