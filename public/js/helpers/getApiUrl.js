923e75d875842c5ae3613700581e446818de8cf5
limberMay <limbermay1@gmail.com>
2023-01-08 20:21:28 -0600

    Se arregló la llamada al API

--- public/js/helpers/getApiUrl.js
+++ public/js/helpers/getApiUrl.js
@@ -2,7 +2,7 @@
 export const getApiUrl = () => {
     let apiUrl = (window.location.hostname.includes('localhost')) 
     ? 'http://localhost'
-    : 'https://webnoteseasy.up.railway.app/';
+    : 'https://webnoteseasy.up.railway.app';
 
     return apiUrl;
 }
